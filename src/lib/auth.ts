606268ee5f59e047ea8d8d36c491320a8979c865
ED-LetsCode <eray.demirel@lets-code.at>
2023-06-29 22:28:07 +0200

    Added nextauth

---
+++ src/lib/auth.ts
@@ -0,0 +1,17 @@
+import { NextAuthOptions } from "next-auth";
+import AzureADProvider from "next-auth/providers/azure-ad";
+
+export const authOptions: NextAuthOptions = {
+  // https://next-auth.js.org/configuration/providers
+  providers: [
+    AzureADProvider({
+      clientId: process.env.AZURE_AD_CLIENT_ID!,
+      clientSecret: process.env.AZURE_AD_CLIENT_SECRET!,
+      tenantId: process.env.AZURE_AD_TENANT_ID,
+    }),
+  ],
+  secret: process.env.SECRET,
+  session: {
+    strategy: "jwt",
+  },
+};
