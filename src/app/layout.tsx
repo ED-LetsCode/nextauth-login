186c6a83e79d1468b6413c1f80bdc716993be44f
ED-LetsCode <eray.demirel@lets-code.at>
2023-06-29 21:05:35 +0200

    Update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import Navbar from "@/components/Navbar";
+import { cn } from "@/lib/utils";
 import "@/styles/globals.css";
 import { Inter } from "next/font/google";
 
@@ -15,7 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`bg-slate-50 ${cn(inter.className)}`}>
+        <Navbar />
+        <div className="container ">{children}</div>
+      </body>
     </html>
   );
 }
