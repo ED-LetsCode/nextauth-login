e9a6887bff80d17e52de0b2c5f6200e7bec80e01
ED-LetsCode <eray.demirel@lets-code.at>
2023-07-01 15:16:23 +0200

    Update

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/Avatar";
 import { Button } from "@/components/ui/Button";
 import {
@@ -9,30 +11,41 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/DropdownMenu";
 import { Icons } from "./Icons";
+import { useSession, signOut } from "next-auth/react";
 
 const UserProfile = () => {
+  const { data: session } = useSession();
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" className="relative h-8 w-8 rounded-full">
-          <Avatar className="h-10 w-10 border border-black">
-            <AvatarImage src="/avatars/01.png" alt="@shadcn" />
-            <AvatarFallback>ED</AvatarFallback>
+          <Avatar className="h-10 w-10  ">
+            <AvatarImage src={session?.user?.image || ""} alt="" />
+            <AvatarFallback>{session?.user?.name?.charAt(0)}</AvatarFallback>
           </Avatar>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56" align="end" forceMount>
         <DropdownMenuLabel className="font-normal">
           <div className="flex flex-col gap-y-2">
-            <p className="text-sm font-medium leading-none">Eray</p>
+            <p className="text-sm font-medium leading-none">
+              {session?.user?.name}
+            </p>
             <p className="text-xs leading-none text-muted-foreground">
-              Eray.demirel@lets-code.at
+              {session?.user?.email}
             </p>
           </div>
         </DropdownMenuLabel>
 
         <DropdownMenuSeparator />
-        <DropdownMenuItem>
+        <DropdownMenuItem
+          onClick={() =>
+            signOut({
+              callbackUrl: "/login",
+            })
+          }
+        >
           <Icons.logOut className="w-4 h-4 mr-2 " />
           <span>Log out</span>
         </DropdownMenuItem>
