2d74f46789a9b6ae70e9528ced6d12cbc452ffba
ED-LetsCode <eray.demirel@lets-code.at>
2023-07-01 15:16:42 +0200

    Added conditional rendering

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,26 @@
-import { FC } from "react";
 import { Icons } from "./Icons";
 import UserProfile from "./UserProfile";
 import Link from "next/link";
+import ThemeActions from "./theme/ThemeActions";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
 
-const Navbar: FC = () => {
+const Navbar = async () => {
+  const session = await getServerSession(authOptions);
+
   return (
-    <div className="border-b py-3 bg-white shadow-md">
+    <div className="border-b py-3  shadow-md">
       <div className="flex items-center justify-between container">
         <Link href="/">
           <Icons.logo className="h-8 w-8" />
         </Link>
-        <UserProfile />
+        <div className="flex items-center space-x-4">
+          <ThemeActions />
+          {
+            // If there is a session, show the user profile
+            session && <UserProfile />
+          }
+        </div>
       </div>
     </div>
   );
