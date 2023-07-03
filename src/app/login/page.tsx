f5046cf1795a81078f93ba60c48daba0c4ac51ab
ED-LetsCode <eray.demirel@lets-code.at>
2023-07-03 21:32:48 +0200

    Added redirect to home if user is logged in

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,15 @@
 import Login from "@/components/Login";
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-const Page = () => {
+const Page = async () => {
+  const session = await getServerSession(authOptions);
+  // If there is a session, redirect to home page
+  if (session) {
+    redirect("/");
+  }
+
   return (
     <div className="h-full flex justify-center items-center">
       <Login />
